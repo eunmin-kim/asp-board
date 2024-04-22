ad526443933d8e7e86bb2412ccd7eba097baa1b2
eunmin-kim <53runtime@gmail.com>
2024-04-22 17:09:43 +0900

    Add default layout and styles for AspCoreBoardProject
    Added the default layout for AspCoreBoardProject, along with related styles and scripts. This includes the main _Layout.cshtml file, accompanying CSS, and necessary scripts for validation.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspCoreBoardProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
